096e094098e260b8ea8653852034708c47108351
hamupapa <43082939+hamupapa@users.noreply.github.com>
2018-09-13 12:52:48 +0900

    Add generator

--- python/test_study.py
+++ python/test_study.py
@@ -614,4 +614,17 @@ for name, speed in li:
     print(name, speed)
 
 
+print("================ ジェネレータ ================")
+# yield で値を返す関数を定義
+def genlto3();
+    yield 1;
+    yield 2;
+    yield 3;
+    
+# イテレータオブジェクトを得る
+it = genlto3()
+# for構文で繰り返し表示
+for i in it:
+    print(i)
+
 
