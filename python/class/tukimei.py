7ef6c64b094c41bfbdd0f96eb834e58a2094810c
hamupapa <k.mine2012@gmail.com>
2018-11-15 23:09:33 +0900

    class6

---
+++ python/class/tukimei.py
@@ -0,0 +1,11 @@
+class Tukimei:
+    tuki = ["睦月", "如月", "弥生", "卯月", "皐月", "水無月",
+            "文月", "葉月", "長月", "神無月", "霜月", "師走"]
+    def __getitem__(self, key):
+        i = int(key)
+        return self.tuki[i-1]   # Index番号に「-1」を指定して、月と一致させる
+
+# 添え字アクセスしてみる
+t = Tukimei()
+print(t[3])
+print(t[6])
