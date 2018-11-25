131e58b6b3014bdff557bf235b908b06a5ec4820
hamupapa <k.mine2012@gmail.com>
2018-11-25 23:44:06 +0900

    scraping 3

--- python/scraping/sample_scraping2.py
+++ python/scraping/sample_scraping2.py
@@ -2,7 +2,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://news.yahoo.co.jp/')	# ページの情報を取得
+#r = requests.get('https://news.yahoo.co.jp/')	# ページの情報を取得
+r = requests.get('https://www.nikkei.com/')	# ページの情報を取得
 
 #print(r.headers)
 #print("------------")
@@ -11,12 +12,16 @@ r = requests.get('https://news.yahoo.co.jp/')	# ページの情報を取得
 
 soup = BeautifulSoup(r.content, "html.parser")
 
-print(soup.h1)				# 最初のタグ要素の取得
-print(soup.h1.attrs)		# h1タグの属性を取得
-print(soup.h1['class'])		# h1タグのclassを取得
-print(soup.h1.name)			# h1タグのタグ名を取得
-print(soup.p)				# 最初のdivタグを取得
-print(soup.div.p)			# divタグの次のpタグを取得
-print(soup.div.p.string)	# divタグの次のpタグの内容を取得
+try:
+	print("1", soup.find("a"))			# 最初のaタグ要素の取得
+	print("2", soup.a)					# 最初のaタグ要素の取得
+	print("3", soup.find("a").attrs)	# aタグの属性を取得
+	print("4", soup.a.attrs)			# aタグの属性を取得
+	print("5", soup.a['class'])			# aタグのclassを取得
+	print("6", soup.a.name)				# aタグのタグ名を取得
+	print("7", soup.a.span)				# aタグの次のpタグを取得
+	print("8", soup.a.span.string)		# divタグの次のpタグの内容を取得
+except:
+	pass
 
 
