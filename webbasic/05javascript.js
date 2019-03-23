69a409569786d7a23d06800e862382c0fac5ed7c
hamupapa <43082939+hamupapa@users.noreply.github.com>
2019-03-24 00:02:51 +0900

    Add files via upload

---
+++ webbasic/05javascript.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// コンソールログを出す
+var name = "CODEPREP";
+console.log('Hello ' + name);
+
+// JavaScriptの型
+/* 文字列 */
+var string = "Hello";
+console.log(string);
+
+/* 数値 */
+var number = 10;
+console.log(number);
+
+/* 小数 */
+var number = 8.7;
+console.log(number);
+
+/* 真偽値 */
+var boolean = true;
+console.log(boolean);
+
+/* 配列 */
+var array = [];		// 配列を定義
+array.push('Welcome to CODEPREP');	// 配列に文字列を追加
+console.log(array[0]);	// 配列の1番目を出力
+
+/* オブジェクト */
+var object = {};	// オブジェクトを定義
+object.name = "CODEPREP";	// nameプロパティを追加し、文字列を設定
+console.log(object.name);	// nameプロパティを出力
+
