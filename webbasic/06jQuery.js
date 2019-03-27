b2d3b8ff994323a2cc0d9dda847200bd1e8e81ef
hamupapa <43082939+hamupapa@users.noreply.github.com>
2019-03-27 22:29:45 +0900

    Add files via upload

--- webbasic/06jQuery.js
+++ webbasic/06jQuery.js
@@ -9,9 +9,27 @@
 		
 		/* ID名から要素を取得する */
 		var elem2 = $("#hello2");
-		elem2.css("backgroundColor", "#9F9");
+		elem2.css("backgroundColor", "#99F");
 		
 		/* クラス名から要素を取得する */
+		var elem3 = $(".elems");
+		elem3.css("backgroundColor", "#F99");
+		
+		/* タグ名から要素を取得する */
+		var elem4 = $("h1");
+		elem4.css("backgroundColor", "#999");
+		
+		/* 最初の要素の選択 */
+		var elem5 = $("li:first");
+		elem5.css("backgroundColor", "#FF9");
+		
+		/* 最後の要素の選択 */
+		var elem5 = $("li:last");
+		elem5.css("backgroundColor", "#9FF");
+		
+		/* 3番目の要素の選択 */
+		var elem5 = $("li:eq(2)");
+		elem5.css("backgroundColor", "#99F");
 	});
 	
 })(jQuery);
